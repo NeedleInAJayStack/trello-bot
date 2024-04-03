13e7056d5b5fa2b91a9d412d68cb3b40dcb95d2c
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-04-02 19:48:02 -0600

    refactor: Adds eslint & formats

--- dev_inspect-trello.js
+++ dev_inspect-trello.js
@@ -1,25 +1,23 @@
+const Trello = require('node-trello') // Doc here: https://www.npmjs.com/package/node-trello
 
-const trello = require('node-trello'); // Doc here: https://www.npmjs.com/package/node-trello
-
-
-const trelloDevKey = process.env.TRELLO_DEV_KEY;
-const jaysTrelloBotToken = process.env.TRELLO_BOT_TOKEN; // This is the bot's token.
-var botTrello = new trello(trelloDevKey, jaysTrelloBotToken); // Connect to Trello
+const trelloDevKey = process.env.TRELLO_DEV_KEY
+const jaysTrelloBotToken = process.env.TRELLO_BOT_TOKEN // This is the bot's token.
+const botTrello = new Trello(trelloDevKey, jaysTrelloBotToken) // Connect to Trello
 
 // // Gets the boards of the user.
-// botTrello.get(`/1/members/jaysuserbot/boards`, function(err, data) {
-//   if (err) throw err;
-//   console.log(data);
-// });
+botTrello.get('/1/members/jaysuserbot/boards', function (err, data) {
+  if (err) throw err
+  console.log(data)
+})
 
 // // Gets the lists of the board.
-// botTrello.get(`/1/boards/649fc332c93c2d7139afa8c0/lists`, function(err, data) {
-//   if (err) throw err;
-//   console.log(data);
-// });
+botTrello.get('/1/boards/649fc332c93c2d7139afa8c0/lists', function (err, data) {
+  if (err) throw err
+  console.log(data)
+})
 
 // // Gets the labels of the board.
-// botTrello.get(`/1/boards/649fc332c93c2d7139afa8c0/labels`, function(err, data) {
-//   if (err) throw err;
-//   console.log(data);
-// });
+botTrello.get('/1/boards/649fc332c93c2d7139afa8c0/labels', function (err, data) {
+  if (err) throw err
+  console.log(data)
+})
--- server.js
+++ server.js
@@ -1,153 +1,148 @@
-const http = require('http');
-const cron = require('node-cron'); // Doc here: https://www.npmjs.com/package/node-cron
-const trello = require('node-trello'); // Doc here: https://www.npmjs.com/package/node-trello
-const serverStartTime = new Date();
-var serverHeader = "";
-var httpLog = "";
+const http = require('http')
+const cron = require('node-cron') // Doc here: https://www.npmjs.com/package/node-cron
+const Trello = require('node-trello') // Doc here: https://www.npmjs.com/package/node-trello
+const serverStartTime = new Date()
+let serverHeader = ''
+let httpLog = ''
 
 // Create a simple server
-const hostname = '0.0.0.0';
-const port = 3333;
+const hostname = '0.0.0.0'
+const port = 3333
 
 const server = http.createServer((req, res) => {
-  if(req.method === 'GET') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    serverHeader = "Started: "+serverStartTime.toISOString()+"\n\n";
-    res.end(serverHeader+httpLog);
+  if (req.method === 'GET') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    serverHeader = 'Started: ' + serverStartTime.toISOString() + '\n\n'
+    res.end(serverHeader + httpLog)
+  } else {
+    res.statusCode = 405
   }
-  else {
-    res.statusCode = 405;
-  }
-});
+})
 
 server.listen(port, hostname, () => {
-  console.log("Server running at http://"+hostname+":"+port+"/");
-});
+  console.log('Server running at http://' + hostname + ':' + port + '/')
+})
 
 // Adds the specified message to the top of the http log.
-var httpLogAdd = function(toAdd){
-  var date = new Date();
-  httpLog = date.toISOString()+" - "+ toAdd + "\n" + httpLog; // Add it to the top.
-};
+const httpLogAdd = function (toAdd) {
+  const date = new Date()
+  httpLog = date.toISOString() + ' - ' + toAdd + '\n' + httpLog // Add it to the top.
+}
 
 // TRELLO
-const trelloDevKey = process.env.TRELLO_DEV_KEY;
-const jaysTrelloBotToken = process.env.TRELLO_BOT_TOKEN; // This is the bot's token.
-var botTrello = new trello(trelloDevKey, jaysTrelloBotToken); // Connect to Trello
-
+const trelloDevKey = process.env.TRELLO_DEV_KEY
+const jaysTrelloBotToken = process.env.TRELLO_BOT_TOKEN // This is the bot's token.
+const botTrello = new Trello(trelloDevKey, jaysTrelloBotToken) // Connect to Trello
 
 // This function tries to find the card to be created by name. If it doesn't exist, it creates it. If it does, but in
 // a different list, it moves it, and if it exists in the same list, it is commented on.
-var staticScheduleCardFunc = function(newCard) {
-  var date = new Date();
-  if(newCard.dayRange === undefined || newCard.dayRange.indexOf(date.getDate()) > -1) { // Only proceed if dayRange is undefined or today is within the range.
-    botTrello.get(`/1/boards/${newCard.idBoard}/cards/`, function(err, data) {
-      if (err) throw err;
+const staticScheduleCardFunc = function (newCard) {
+  const date = new Date()
+  if (newCard.dayRange === undefined || newCard.dayRange.indexOf(date.getDate()) > -1) { // Only proceed if dayRange is undefined or today is within the range.
+    botTrello.get(`/1/boards/${newCard.idBoard}/cards/`, function (err, data) {
+      if (err) throw err
 
-      var existingCard = null;
-      data.forEach(function(card, array, index) { // Find if one with the same name exists
-        if(card.name === newCard.name) existingCard = card; // Match up cards by name.
-      });
-      if(existingCard === null) { // Create the new card
-        botTrello.post(`/1/cards/`, newCard, function(err, data) {
-          if (err) throw err;
-          httpLogAdd(`"${newCard.name}" created successfully`);
-        });
-      }
-      else {
-        if(existingCard.idList !== newCard.idList) { // Move card to the correct list and update to definition
-          botTrello.put(`/1/cards/${existingCard.id}`, newCard, function(err, data) {
-            if (err) throw err;
-            httpLogAdd(`"${existingCard.name}" moved successfully`);
-          });
-        }
-        else { // In this case, it's already there. Just comment.
-          var comment = "Schedule hit again.";
-          botTrello.post(`/1/cards/${existingCard.id}/actions/comments/`, {text: comment}, function(err, data) {
-            if (err) throw err;
-            httpLogAdd(`"${existingCard.name}" commented with "${comment}"`);
-          });
+      let existingCard = null
+      data.forEach(function (card, array, index) { // Find if one with the same name exists
+        if (card.name === newCard.name) existingCard = card // Match up cards by name.
+      })
+      if (existingCard === null) { // Create the new card
+        botTrello.post('/1/cards/', newCard, function (err, data) {
+          if (err) throw err
+          httpLogAdd(`"${newCard.name}" created successfully`)
+        })
+      } else {
+        if (existingCard.idList !== newCard.idList) { // Move card to the correct list and update to definition
+          botTrello.put(`/1/cards/${existingCard.id}`, newCard, function (err, data) {
+            if (err) throw err
+            httpLogAdd(`"${existingCard.name}" moved successfully`)
+          })
+        } else { // In this case, it's already there. Just comment.
+          const comment = 'Schedule hit again.'
+          botTrello.post(`/1/cards/${existingCard.id}/actions/comments/`, { text: comment }, function (err, data) {
+            if (err) throw err
+            httpLogAdd(`"${existingCard.name}" commented with "${comment}"`)
+          })
         }
       }
-    });
+    })
   }
-};
+}
 
 // Monthly date range rules. According to these rules, weeks will occasionally be skipped.
-var firstWeek = [1,2,3,4,5,6,7];
-var secondWeek = [8,9,10,11,12,13,14];
-var thirdWeek = [15,16,17,18,19,20,21];
-var fourthWeek = [22,23,24,25,26,27,28];
-var firstAndThirdWeek = [1,2,3,4,5,6,7, 15,16,17,18,19,20,21];
-var secondAndFourthWeek = [8,9,10,11,12,13,14, 22,23,24,25,26,27,28];
-
+const firstWeek = [1, 2, 3, 4, 5, 6, 7]
+// const secondWeek = [8, 9, 10, 11, 12, 13, 14]
+const thirdWeek = [15, 16, 17, 18, 19, 20, 21]
+const fourthWeek = [22, 23, 24, 25, 26, 27, 28]
+// const firstAndThirdWeek = [1, 2, 3, 4, 5, 6, 7, 15, 16, 17, 18, 19, 20, 21]
+// const secondAndFourthWeek = [8, 9, 10, 11, 12, 13, 14, 22, 23, 24, 25, 26, 27, 28]
 
 // TASKS
 // Board
-var tasksBoardId = "649fc332c93c2d7139afa8c0";
+const tasksBoardId = '649fc332c93c2d7139afa8c0'
 // List
-var tasksToDoListId = "649fc332c93c2d7139afa8c7";
+const tasksToDoListId = '649fc332c93c2d7139afa8c7'
 // Labels
-var tasksScheduledLabelId = "649fc333c93c2d7139afab7f";
+const tasksScheduledLabelId = '649fc333c93c2d7139afab7f'
 
 // Cards
 // Relevant card parameters are defined according to Trello post method: https://developers.trello.com/v1.0/reference#cards-2
 
-var tasksCards = [
+const tasksCards = [
   {
-    name: "Pay Mortgage",
-    desc: "**Schedule**: Monthly on the 25th at 6PM\n"+
-          "\n"+
-          "https://mypennymac.pennymacusa.com/\n"+
-          "\n"+
-          "Venmo request $1,460 from Katelyn\n",
-    cronSchedule: "0 18 25 * *",
+    name: 'Pay Mortgage',
+    desc: '**Schedule**: Monthly on the 25th at 6PM\n' +
+          '\n' +
+          'https://mypennymac.pennymacusa.com/\n' +
+          '\n' +
+          'Venmo request $1,460 from Katelyn\n',
+    cronSchedule: '0 18 25 * *',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Input Utilities",
-    desc: "**Schedule**: Monthly on the 1st at 6PM\n"+
-    "\n"+
-    "Input utility data into PostgreSQL",
-    cronSchedule: "0 18 1 * *",
+    name: 'Input Utilities',
+    desc: '**Schedule**: Monthly on the 1st at 6PM\n' +
+    '\n' +
+    'Input utility data into PostgreSQL',
+    cronSchedule: '0 18 1 * *',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Pay Cell Phone",
-    desc: "**Schedule**: Monthly on the 1st at 6PM\n"+
-          "\n"+
-          "$35 to Mom through PayPal:\n",
-    cronSchedule: "0 18 1 * *",
+    name: 'Pay Cell Phone',
+    desc: '**Schedule**: Monthly on the 1st at 6PM\n' +
+          '\n' +
+          '$35 to Mom through PayPal:\n',
+    cronSchedule: '0 18 1 * *',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Monthly Money Review",
-    desc: "**Schedule**: Monthly on the 28th at 6PM\n"+
-      "\n"+
-      "Review your monthly spending in GnuCash",
-    cronSchedule: "0 18 28 * *",
+    name: 'Monthly Money Review',
+    desc: '**Schedule**: Monthly on the 28th at 6PM\n' +
+      '\n' +
+      'Review your monthly spending in GnuCash',
+    cronSchedule: '0 18 28 * *',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
     name: "Brush Gracie's Teeth",
-    desc: "**Schedule**: Weekly on Saturday at noon",
-    cronSchedule: "0 12 * * 6",
+    desc: '**Schedule**: Weekly on Saturday at noon',
+    cronSchedule: '0 12 * * 6',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   // {
@@ -160,54 +155,54 @@ var tasksCards = [
   //   idLabels: [tasksScheduledLabelId]
   // },
   {
-    name: "Take Out Trash",
-    desc: "**Schedule**: Weekly on Monday at 6PM\n"+
-      "\n"+
-      "Take trash, recycling, and compost cans to the curb",
-    cronSchedule: "0 18 * * 1",
+    name: 'Take Out Trash',
+    desc: '**Schedule**: Weekly on Monday at 6PM\n' +
+      '\n' +
+      'Take trash, recycling, and compost cans to the curb',
+    cronSchedule: '0 18 * * 1',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Bring In Trash",
-    desc: "**Schedule**: Weekly on Tuesday at 6PM\n"+
-      "\n"+
-      "Bring in trash, recycling, and compost cans from the curb",
-    cronSchedule: "0 18 * * 2",
+    name: 'Bring In Trash',
+    desc: '**Schedule**: Weekly on Tuesday at 6PM\n' +
+      '\n' +
+      'Bring in trash, recycling, and compost cans from the curb',
+    cronSchedule: '0 18 * * 2',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Water Indoor Plants",
-    desc: "**Schedule**: Weekly on Tuesday at 6PM",
-    cronSchedule: "0 18 * * 2",
+    name: 'Water Indoor Plants',
+    desc: '**Schedule**: Weekly on Tuesday at 6PM',
+    cronSchedule: '0 18 * * 2',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Mow Lawn",
-    desc: "**Schedule**: Every Thursday at 6PM from April through October",
-    cronSchedule: "0 18 * 4-10 4",
+    name: 'Mow Lawn',
+    desc: '**Schedule**: Every Thursday at 6PM from April through October',
+    cronSchedule: '0 18 * 4-10 4',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Pool Maintenance",
-    desc: "**Schedule**: Every Saturday at noon from May through October\n"+
-      "\n"+
-      "Perform pool testing, chemical treatments, straining, brushing, and vacuuming",
-    cronSchedule: "0 12 * 5-10 6",
+    name: 'Pool Maintenance',
+    desc: '**Schedule**: Every Saturday at noon from May through October\n' +
+      '\n' +
+      'Perform pool testing, chemical treatments, straining, brushing, and vacuuming',
+    cronSchedule: '0 12 * 5-10 6',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   // {
@@ -222,14 +217,14 @@ var tasksCards = [
   //   idLabels: [tasksScheduledLabelId]
   // },
   {
-    name: "Water Backyard Garden",
-    desc: "**Schedule**: Weekly on Sunday and Wednesday at 6PM from April through October\n"+
-      "\n"+
-      "Water garden near the pool and bamboo",
-    cronSchedule: "0 18 * 4-10 0,3",
+    name: 'Water Backyard Garden',
+    desc: '**Schedule**: Weekly on Sunday and Wednesday at 6PM from April through October\n' +
+      '\n' +
+      'Water garden near the pool and bamboo',
+    cronSchedule: '0 18 * 4-10 0,3',
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   // {
@@ -274,59 +269,59 @@ var tasksCards = [
   //   idLabels: [tasksScheduledLabelId]
   // },
   {
-    name: "Clean Bathrooms",
-    desc: "**Schedule**: Monthly on the 3nd Saturday at noon\n"+
-      "\n"+
-      "Check the following and perform as needed:\n"+
-      "\n"+
-      "* Clean mirrors\n"+
-      "* Clean tub\n"+
-      "* Clean sinks\n"+
-      "* Clean toilets\n"+
-      "* Sweep/Vacuum\n"+
-      "* Mop",
-    cronSchedule: "0 12 * * 6",
+    name: 'Clean Bathrooms',
+    desc: '**Schedule**: Monthly on the 3nd Saturday at noon\n' +
+      '\n' +
+      'Check the following and perform as needed:\n' +
+      '\n' +
+      '* Clean mirrors\n' +
+      '* Clean tub\n' +
+      '* Clean sinks\n' +
+      '* Clean toilets\n' +
+      '* Sweep/Vacuum\n' +
+      '* Mop',
+    cronSchedule: '0 12 * * 6',
     dayRange: thirdWeek,
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Clean Living Room",
-    desc: "**Schedule**: Monthly on the 4th Saturday at noon\n"+
-      "\n"+
-      "Check the following and perform as needed:\n"+
-      "\n"+
-      "* Pick up clutter\n"+
-      "* Dust\n"+
-      "* Clean glass tables\n"+
-      "* Sweep/Vacuum",
-    cronSchedule: "0 12 * * 6",
+    name: 'Clean Living Room',
+    desc: '**Schedule**: Monthly on the 4th Saturday at noon\n' +
+      '\n' +
+      'Check the following and perform as needed:\n' +
+      '\n' +
+      '* Pick up clutter\n' +
+      '* Dust\n' +
+      '* Clean glass tables\n' +
+      '* Sweep/Vacuum',
+    cronSchedule: '0 12 * * 6',
     dayRange: fourthWeek,
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   },
   {
-    name: "Clean Den",
-    desc: "**Schedule**: Monthly on the 1st Saturday at noon\n"+
-      "\n"+
-      "Check the following and perform as needed:\n"+
-      "\n"+
-      "* Pick up clutter\n"+
-      "* Dust\n"+
-      "* Clean glass tables\n"+
-      "* Sweep/Vacuum",
-    cronSchedule: "0 12 * * 6",
+    name: 'Clean Den',
+    desc: '**Schedule**: Monthly on the 1st Saturday at noon\n' +
+      '\n' +
+      'Check the following and perform as needed:\n' +
+      '\n' +
+      '* Pick up clutter\n' +
+      '* Dust\n' +
+      '* Clean glass tables\n' +
+      '* Sweep/Vacuum',
+    cronSchedule: '0 12 * * 6',
     dayRange: firstWeek,
     idBoard: tasksBoardId,
     idList: tasksToDoListId,
-    pos:"top",
+    pos: 'top',
     idLabels: [tasksScheduledLabelId]
   }
 ]
-tasksCards.forEach( card => {
-  cron.schedule(card.cronSchedule, function(){staticScheduleCardFunc(card);});
-});
+tasksCards.forEach(card => {
+  cron.schedule(card.cronSchedule, function () { staticScheduleCardFunc(card) })
+})
